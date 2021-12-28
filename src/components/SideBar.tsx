448c56d88f18f15e921f4ad6f17d27cd560020ef
Matheus Teixeira da Silva <matheusts.dev@gmail.com>
2021-12-28 17:25:24 -0300

    Desafio concluído

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,34 @@
-export function SideBar() {
-  // Complete aqui
+import { GenreResponseProps } from "../App";
+import { Button } from "./Button";
+
+interface SideBarProps {
+  genreList: GenreResponseProps[];
+  selectedGenreId: number;
+  handleSelectGenre: (id: number) => void;
+}
+
+export function SideBar({
+  genreList,
+  handleSelectGenre,
+  selectedGenreId,
+}: SideBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genreList.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleSelectGenre(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
